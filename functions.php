1125cbd86ad0a10e31ba1ab0f10c07078793086a
aerovir <aeroviradmin@gmail.com>
2018-05-22 16:48:23 +0200

    первая

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aerovir
+ * Date: 21.05.2018
+ * Time: 21:47
+ */
---
+++ templates/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aerovir
+ * Date: 22.05.18
+ * Time: 9:54
+ */
